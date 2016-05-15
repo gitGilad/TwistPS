5b9ee812e1f35600a727baef5fbba75da0b7f366
root <egilad86@gmail.com>
2016-05-15 19:57:43 +0300

    Fix: CPU time was calculated twice, change sizes to long Clean Code: Renaming, decrease scopes

--- src/TwistlockPS.cpp
+++ src/TwistlockPS.cpp
@@ -12,15 +12,16 @@
 long get_process_cpu_time(long pid) {
 
 	char path[40], line[300];
-	FILE* statf;
 
 	snprintf(path, 40, "/proc/%ld/stat", pid);
 
+	FILE* statf;
 	statf = fopen(path, "r");
 	if(!statf)
 		return -1;
 
 	fgets(line, 300, statf);
+	fclose(statf);
 
 	long user, kernel;
 	sscanf(line, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*lu %*lu %*lu %*lu "
@@ -28,34 +29,33 @@ long get_process_cpu_time(long pid) {
 			"%*ld %*ld %*ld %*ld %*ld %*ld %*llu %*lu",
 			&user, &kernel);
 
-	fclose(statf);
 	return user + kernel;
 }
 
 long get_total_cpu_time() {
-	  char line[300];
+
 	FILE* statf;
 
 	statf = fopen("/proc/stat", "r");
 	if(!statf)
 		return -1;
 
-	long user, nice, system, idle, total;
+	char line[300];
+	fgets(line, 300, statf);
+	fclose(statf);
 
-	while(fgets(line, 300, statf)) {
-		if(strncmp(line, "cpu", 3) == 0) {
-			sscanf(line, "%*s %lu %lu %lu %lu", &user, &nice, &system, &idle);
-		}
-
-		total = user + nice + system + idle;
+	if(strncmp(line, "cpu", 3) != 0) {
+	    perror("Error while retrieving cpu time");
+	    return -1;
 	}
 
+	long user, nice, system, idle, iowait, irq, softirq;
+	sscanf(line, "%*s %lu %lu %lu %lu %lu %lu %lu", &user, &nice, &system, &idle, &iowait, &irq, &softirq);
 
-	fclose(statf);
-	return total;
+	return user + nice + system + idle + iowait + irq + softirq;
 }
 
-void get_cpu_times(std::map<long, int> & cpu_before_times) {
+void get_cpu_times(std::map<long, long> & cpu_before_times) {
 
 	DIR* proc = opendir("/proc");
 	struct dirent* ent;
@@ -71,11 +71,10 @@ void get_cpu_times(std::map<long, int> & cpu_before_times) {
 
 	    long pid = strtol(ent->d_name, NULL, 10);
 
-	    cpu_before_times.insert(std::pair<long, int>(pid, get_process_cpu_time(pid)));
+	    cpu_before_times.insert(std::pair<long, long>(pid, get_process_cpu_time(pid)));
 	}
 
-	cpu_before_times.insert(std::pair<long, int>(-1, get_total_cpu_time()));
-
+	cpu_before_times.insert(std::pair<long, long>(-1, get_total_cpu_time()));
 	closedir(proc);
 }
 
@@ -120,21 +119,21 @@ void parse_memory_line(char* line, char* memory_size) {
 	strncpy(memory_size, p, memory_string_value_size);
 }
 
-double calculate_cpu(std::map<long, int> cpu_before_times, long pid) {
-	std::map<long, int>::iterator it = cpu_before_times.find(pid);
+double calculate_cpu(std::map<long, long> cpu_before_times, long pid) {
+	std::map<long, long>::iterator pid_before_time_it = cpu_before_times.find(pid);
 
-	if (it == cpu_before_times.end()) {
+	if (pid_before_time_it == cpu_before_times.end()) {
 		return -1;
 	}
 
-	long ptime = get_process_cpu_time(pid) - it->second;
+	long ptime = get_process_cpu_time(pid) - pid_before_time_it->second;
 	long ttime = get_total_cpu_time() - cpu_before_times.find(-1)->second;
 
 	return 100 * ptime / ttime;
 }
 
 
-void print_status(long pid, std::map<long, int> cpu_before_times) {
+void print_status(long pid, std::map<long, long> cpu_before_times) {
     char path[40], line[100], pname[100], uid[50], memory[20];
     FILE* statusf;
 
@@ -147,7 +146,7 @@ void print_status(long pid, std::map<long, int> cpu_before_times) {
     bool vm_size_is_set = false;
 
     while(fgets(line, 100, statusf)) {
-//
+
         if(strncmp(line, "Name:", 5) == 0) {
 
         	std::fill_n(pname, 100, 0);
@@ -166,7 +165,7 @@ void print_status(long pid, std::map<long, int> cpu_before_times) {
 	if (!vm_size_is_set) {
 		strcpy(memory, "0.0");
 	}
-	printf("|%-18s| %12s| %-20s| %-10.3f|\n",
+	printf("|%-18s| %12s| %-20s| %-11.3f|\n",
 			get_user_name(strtol(uid, NULL, 10)),
 			memory,
 			pname,
@@ -175,7 +174,7 @@ void print_status(long pid, std::map<long, int> cpu_before_times) {
     fclose(statusf);
 }
 
-void iterate_proc_and_print_info(std::map<long, int> before_times) {
+void iterate_proc_and_print_info(std::map<long, long> before_times) {
 
 	DIR* proc = opendir("/proc");
 	struct dirent* ent;
@@ -196,14 +195,14 @@ void iterate_proc_and_print_info(std::map<long, int> before_times) {
 }
 
 void print_header() {
-	printf("|%-18s| %-12s| %-20s| %-10s|\n", "User Name", "Memory", "Process Name", "CPU Usage");
-	printf("---------------------------------------------------------\n");
+	printf("|%-18s| %-12s| %-20s| %-10s|\n", "User Name", "Memory", "Process Name", "CPU % Usage");
+	printf("---------------------------------------------------------------------\n");
 }
 
 
 int main(int argc, char *argv[])
 {
-	std::map<long, int> cpu_before_times;
+	std::map<long, long> cpu_before_times;
 	get_cpu_times(cpu_before_times);
 	sleep(1);
 	print_header();
